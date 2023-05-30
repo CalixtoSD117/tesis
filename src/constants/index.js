2065225ba35a0a69fed7e182d8fd3fb053bf23ae
CalixtoSD117 <ccalixtoengelberger117@gmail.com>
2023-05-30 10:15:51 -0600

    data

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,18 +1,18 @@
-import { persona1 } from "../assets";
+import { p6 } from "../assets";
 import { amazon, logmanza, mercado, ragel } from "../assets";
 
 
 export const user =[
     {
-        name: 'Alina Virginia',
-        lastname: 'Parra Megia',
-        gmail: 'Alina@example.com',
+        name: 'Juan Pablo ',
+        lastname: 'Gomez Ochoa',
+        gmail: 'Juan@ucol.mx',
         role: 'Administrador',
         empresa: 'Facebook',
         dispositivos: '1',
         puerto: 'Manzanillo',
         area:'A1',
-        img: persona1,
+        img: p6,
     }
 ]
 
