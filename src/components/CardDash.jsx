be38da4d25e4f88a35f9971c069de75bf81efe2b
CalixtoSD117 <ccalixtoengelberger117@gmail.com>
2023-05-30 10:28:56 -0600

    changes in dashboard and devices

--- src/components/CardDash.jsx
+++ src/components/CardDash.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useState, useEffect  } from 'react';
 
 const CardDash = () =>
 {
+    const [randomNumber, setRandomNumber] = useState(null);
+    const [randomNumber2, setRandomNumber2] = useState(null);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+          const newNumber = Math.floor(Math.random() * 3) + 1;
+          setRandomNumber(newNumber);
+        }, 2000); // Cambia el número cada 2 segundos (2000 milisegundos)
+    
+        return () => clearInterval(interval);
+      }, []);
+
+      useEffect(() => {
+        const interval = setInterval(() => {
+          const newNumber = Math.floor(Math.random() * 2) + 1;
+          setRandomNumber2(newNumber);
+        }, 2000); // Cambia el número cada 2 segundos (2000 milisegundos)
+    
+        return () => clearInterval(interval);
+      }, []);
+
     return (
         <>
             <div className="ring-1 ring-white p-4 rounded-lg bg-white shadow-lg w-full lg:mb-0 mb-4 ">
@@ -20,8 +42,8 @@ const CardDash = () =>
                 <div className="bg-white shadow-lg ring-1 ring-white p-4 rounded-lg   w-full"  >
                     <div>
                         <h1 className="text-xl lg:text-2xl font-bold pb-2 ">Dispositivos</h1>
-                        <p className="pb-5">En linea: </p>
-                        <p className="pb-5">No en linea: </p>
+                        <p className="pb-5">En linea: {randomNumber} </p>
+                        <p className="pb-5">No en linea: {randomNumber2} </p>
                         <br />
                     </div>
 
